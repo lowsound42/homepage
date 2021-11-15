c7a2d59200db970c1581ad5db0f4dbf1b58c3327
Omar Khan <o.khandxb@gmail.com>
2021-11-15 01:32:08 -0500

    styled components config

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,36 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document from 'next/document';
+import { Head, Html, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
-class MyDocument extends Document {
+export default class MyDocument extends Document {
     static async getInitialProps(ctx: any) {
-        const initialProps = await Document.getInitialProps(ctx);
-        return { ...initialProps };
-    }
+        const sheet = new ServerStyleSheet();
+        const originalRenderPage = ctx.renderPage;
 
+        try {
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    enhanceApp: (App: any) => (props: any) =>
+                        sheet.collectStyles(<App {...props} />)
+                });
+
+            const initialProps = await Document.getInitialProps(ctx);
+            return {
+                ...initialProps,
+                styles: (
+                    <>
+                        {initialProps.styles}
+                        {sheet.getStyleElement()}
+                    </>
+                )
+            };
+        } finally {
+            sheet.seal();
+        }
+    }
     render() {
         return (
-            <Html>
+            <Html lang="en">
                 <Head>
                     <link
                         href="https://fonts.googleapis.com/css2?family=Space+Grotesk&family=Space+Mono&display=swap"
@@ -23,5 +45,3 @@ class MyDocument extends Document {
         );
     }
 }
-
-export default MyDocument;
