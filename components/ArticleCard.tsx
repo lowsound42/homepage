f6f71fddaecbbe6a5483c550c5d8758f1a3bcf6e
Omar Khan <o.khandxb@gmail.com>
2021-11-15 01:17:58 -0500

    deployment error fix

--- components/ArticleCard.tsx
+++ components/ArticleCard.tsx
@@ -1,4 +1,5 @@
-import IArticle from '../interfaces/IArticle';
+import styled from 'styled-components';
+
 interface IArticleProps {
     title: string;
     time: string;
@@ -10,8 +11,14 @@ const ArticleCard = ({ title, time, description }: IArticleProps) => {
             <h3>{title}</h3>
             <p>{new Date(time).toLocaleString('en-GB')}</p>
             <p>{description}</p>
-            <p>Read More</p>
+            <ReadMore>Read More</ReadMore>
         </>
     );
 };
 export default ArticleCard;
+
+const ReadMore = styled.p`
+    &:hover {
+        cursor: pointer;
+    }
+`;
--- pages/project/slug.tsx
+++ pages/project/slug.tsx
@@ -1,48 +1,11 @@
 import fs from 'fs';
-import path from 'path';
-import { GetStaticProps, GetStaticPaths } from 'next';
-import { ParsedUrlQuery } from 'querystring';
-import IArticle from '../../interfaces/IArticle';
-import { getAllPosts, getArticleFromCache } from '../../utilities/devTo';
-const cacheFile = './blogcache.json';
-import ReactMarkdown from 'react-markdown';
 
-interface IParams extends ParsedUrlQuery {
-    slug: string;
-}
-
-const Post = (article: IArticle) => {
+const Post = () => {
     return (
         <>
-            <h3>{article.title}</h3>
-            <ReactMarkdown>{article.body_markdown}</ReactMarkdown>
+            <h3>Test</h3>
         </>
     );
 };
 
 export default Post;
-
-export const getStaticProps: GetStaticProps = async (context) => {
-    const { slug } = context.params as IParams;
-    const blogCache = fs.readFileSync(
-        path.join(process.cwd(), cacheFile),
-        'utf-8'
-    );
-    const cache = JSON.parse(blogCache);
-
-    const article: IArticle = await getArticleFromCache(cache, slug);
-    return { props: article };
-};
-
-export const getStaticPaths: GetStaticPaths = async () => {
-    const data = await getAllPosts();
-    fs.writeFileSync(path.join(process.cwd(), cacheFile), JSON.stringify(data));
-
-    const paths = data.map(({ slug }: IParams) => {
-        return {
-            params: { slug }
-        };
-    });
-
-    return { paths, fallback: false };
-};
